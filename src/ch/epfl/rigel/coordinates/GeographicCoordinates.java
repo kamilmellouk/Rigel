15682c54b5d88716326a3ef7380ac7ec9e1cd4f9
kamilmellouk <kamilmellouk@gmail.com>
2020-02-20 15:04:42 +0100

    HorizontalCoordinates commencé

--- src/ch/epfl/rigel/coordinates/GeographicCoordinates.java
+++ src/ch/epfl/rigel/coordinates/GeographicCoordinates.java
@@ -4,6 +4,8 @@ import ch.epfl.rigel.math.Angle;
 import ch.epfl.rigel.math.ClosedInterval;
 import ch.epfl.rigel.math.RightOpenInterval;
 
+import java.util.Locale;
+
 /**
  * @author Mohamed Kamil MELLOUK
  * 19.02.20
@@ -14,6 +16,12 @@ public final class GeographicCoordinates extends SphericalCoordinates {
         super(longitude, latitude);
     }
 
+    /**
+     * Creating GeographicCoordinates from given longitude and latitude in deg
+     * @param lonDeg longitude in deg
+     * @param latDeg latitude in deg
+     * @return GeographicCoordinates of given longitude and latitude in deg
+     */
     GeographicCoordinates ofDeg(double lonDeg, double latDeg) {
         if(isValidLongDeg(lonDeg) && isValidLatDeg(latDeg)){
             return new GeographicCoordinates(lonDeg, latDeg);
@@ -22,33 +30,47 @@ public final class GeographicCoordinates extends SphericalCoordinates {
         }
     }
 
+    /**
+     * Checks if lonDeg is contained in [-180, 180[
+     * @param lonDeg longitude in deg to check
+     * @return boolean telling whether the longitude is contained in [-180, 180[
+     */
     public static boolean isValidLongDeg(double lonDeg) {
         return RightOpenInterval.of(-180,180).contains(lonDeg);
     }
 
+    /**
+     * Checks if latDeg is contained in [-90, 90]
+     * @param latDeg latitude in deg to check
+     * @return boolean telling us whether the longitude is contained in [-90, 90]
+     */
     public static boolean isValidLatDeg(double latDeg) {
         return ClosedInterval.of(-90,90).contains(latDeg);
     }
 
 
-    /*
     @Override
     public double lon() {
-        return longitude;
+        return super.lon();
     }
 
-
+    @Override
     double lonDeg() {
-        return Angle.toDeg(longitude);
+        return super.lonDeg();
     }
 
+    @Override
     double lat() {
-        return latitude;
+        return super.lat();
     }
 
-
+    @Override
     double latDeg() {
-        return Angle.toDeg(latitude);
+        return super.latDeg();
     }
-    */
+
+    @Override
+    public String toString() {
+        return String.format(Locale.ROOT, "(lon=%.4f°, lat=%.4f°)",lonDeg(), latDeg());
+    }
 }
---
+++ src/ch/epfl/rigel/coordinates/HorizontalCoordinates.java
@@ -0,0 +1,50 @@
+package ch.epfl.rigel.coordinates;
+
+import ch.epfl.rigel.math.Angle;
+import ch.epfl.rigel.math.ClosedInterval;
+import ch.epfl.rigel.math.RightOpenInterval;
+
+/**
+ * @author Mohamed Kamil MELLOUK
+ * 20.02.20
+ */
+public final class HorizontalCoordinates extends SphericalCoordinates {
+
+    private HorizontalCoordinates(double longitude, double latitude) {
+        super(longitude, latitude);
+    }
+
+    public static HorizontalCoordinates of(double az, double alt) {
+        if(RightOpenInterval.of(0, Angle.TAU).contains(az) && ClosedInterval.of(-Math.PI/2, Math.PI/2).contains(alt)){
+            return new HorizontalCoordinates(az, alt);
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static HorizontalCoordinates ofDeg(double azDeg, double altDeg) {
+        if(RightOpenInterval.of(0, 360).contains(azDeg) && ClosedInterval.of(-90,90).contains(altDeg)) {
+            return new HorizontalCoordinates(azDeg, altDeg);
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public double az() {
+        return super.lon();
+    }
+
+    double azDeg() {
+        return super.lonDeg();
+    }
+
+    double alt() {
+        return super.lat();
+    }
+
+    double altDeg() {
+        return super.latDeg();
+    }
+
+
+}
