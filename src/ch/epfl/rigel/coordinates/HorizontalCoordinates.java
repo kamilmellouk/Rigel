6e8db55cdb81edea0fa8c635929ff0e7fe34a921
Bastien Faivre <bastien.faivre@epfl.ch>
2020-02-22 15:47:01 +0100

    coordinates

--- src/ch/epfl/rigel/coordinates/HorizontalCoordinates.java
+++ src/ch/epfl/rigel/coordinates/HorizontalCoordinates.java
@@ -4,6 +4,8 @@ import ch.epfl.rigel.math.Angle;
 import ch.epfl.rigel.math.ClosedInterval;
 import ch.epfl.rigel.math.RightOpenInterval;
 
+import java.util.Locale;
+
 /**
  * @author Bastien Faivre (310929)
  * @author Kamil Mellouk (312327)
@@ -64,27 +66,48 @@ public final class HorizontalCoordinates extends SphericalCoordinates {
     /**
      * @return the azimuth in deg
      */
-    double azDeg() {
+    public double azDeg() {
         return super.lonDeg();
     }
 
+    /**
+     * @param n the north
+     * @param e the east
+     * @param s the south
+     * @param w the west
+     * @return the octant where the azimuth is
+     */
+    // TODO: 22/02/2020 limit angles?
     public String azOctantName(String n, String e, String s, String w) {
-        return "";
+        int index = (int) (az() / (Math.PI / 4)) + ((int) ((az() % (Math.PI / 4)) / (Math.PI / 8)) == 0 ? 0 : 1);
+        return new String[]{n, n + e, e, s + e, s, s + w, w, w + n}[index == 8 ? 0 : index];
     }
 
     /**
      * @return the altitude in rad
      */
-    double alt() {
+    public double alt() {
         return super.lat();
     }
 
     /**
      * @return the altitude in deg
      */
-    double altDeg() {
+    public double altDeg() {
         return super.latDeg();
     }
 
+    /**
+     * @param that the given point
+     * @return the angular distance with the given point
+     */
+    public double angularDistanceTo(HorizontalCoordinates that) {
+        return Math.acos(Math.sin(alt()) * Math.sin(that.alt()) + Math.cos(alt()) * Math.cos(that.alt()) * Math.cos(az() - that.az()));
+    }
+
+    @Override
+    public String toString() {
+        return String.format(Locale.ROOT, "(az=%.4f°, alt=%.4f°)", azDeg(), altDeg());
+    }
 
 }
