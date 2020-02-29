d4b74a55c20592ae177353d97c7988cd4ef480a4
Bastien Faivre <bastien.faivre@epfl.ch>
2020-02-29 10:49:12 +0100

    Epoch checked

--- src/ch/epfl/rigel/astronomy/Epoch.java
+++ src/ch/epfl/rigel/astronomy/Epoch.java
@@ -7,9 +7,16 @@ import java.time.temporal.ChronoUnit;
  * @author Bastien Faivre (310929)
  * @author Kamil Mellouk (312327)
  */
+
 public enum Epoch {
-    J2000(ZonedDateTime.of(LocalDate.of(2000, Month.JANUARY, 1), LocalTime.of(12, 0), ZoneOffset.UTC)),
-    J2010(ZonedDateTime.of(LocalDate.of(2010, Month.JANUARY, 1).minusDays(1), LocalTime.of(0,0), ZoneOffset.UTC));
+
+    J2000(ZonedDateTime.of(LocalDate.of(2000, Month.JANUARY, 1),
+            LocalTime.of(12, 0),
+            ZoneOffset.UTC)),
+
+    J2010(ZonedDateTime.of(LocalDate.of(2010, Month.JANUARY, 1).minusDays(1),
+            LocalTime.of(0, 0),
+            ZoneOffset.UTC));
 
     private ZonedDateTime date;
 
@@ -18,20 +25,22 @@ public enum Epoch {
     }
 
     /**
-     * Computing the difference between an Epoch and a given date-time couple
-     * @param when date-time couple to compare
+     * Computing the difference between an Epoch and a given date-time couple with time zone
+     *
+     * @param when date-time couple (with time zone) to compare
      * @return difference in days (can be decimal)
      */
     public double daysUntil(ZonedDateTime when) {
-        return date.until(when, ChronoUnit.MILLIS)/((double)24*60*60*1000);
+        return date.until(when, ChronoUnit.MILLIS) / ((double) 24 * 60 * 60 * 1000);
     }
 
     /**
-     * Computing the difference between an Epoch and a given date-time couple
-     * @param when date-time couple to compare
+     * Computing the difference between an Epoch and a given date-time couple with time zone
+     *
+     * @param when date-time couple (with time zone) to compare
      * @return difference in julian centuries (can be decimal)
      */
     public double julianCenturiesUntil(ZonedDateTime when) {
-        return daysUntil(when)/(double)36525;
+        return daysUntil(when) / (double) 36525;
     }
 }
