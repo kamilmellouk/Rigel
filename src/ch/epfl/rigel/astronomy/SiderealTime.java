e29e064a8871f3ef5baadfd1cd5355b81a5ebbae
Bastien Faivre <bastien.faivre@epfl.ch>
2020-02-29 14:24:54 +0100

    SiderealTime print added need to do test!

--- src/ch/epfl/rigel/astronomy/SiderealTime.java
+++ src/ch/epfl/rigel/astronomy/SiderealTime.java
@@ -26,19 +26,27 @@ public final class SiderealTime {
      * @param when date-time couple with time zone of observation
      * @return sidereal time (in rad) of this point of observation, with respect to greenwich and J2000
      */
-    // TODO: 29/02/2020 check access
+    // TODO: 29/02/2020 check access and remove print
     public static double greenwich(ZonedDateTime when) {
         // Express the  given time in greenwich (UTC) time zone
         ZonedDateTime whenInUTC = when.withZoneSameInstant(ZoneOffset.UTC);
+        System.out.println("when = "+when);
+        System.out.println("whenInUTC = "+whenInUTC);
+        System.out.println("trunc = "+whenInUTC.truncatedTo(ChronoUnit.DAYS));
 
         // Compute the number of julian centuries between J2000 and the given time expressed in UTC
         double T = Epoch.J2000.julianCenturiesUntil(whenInUTC.truncatedTo(ChronoUnit.DAYS));
+        System.out.println("T = "+T);
         // Compute the number of hours between the beginning of the day containing the specific moment and the moment itself
         double t = when.getHour();
+        System.out.println("t = "+t);
 
         // compute the two values of the given polynomial at T and t
         double s0 = Polynomial.of(0.000025862, 2400.051336, 6.697374558).at(T);
+        System.out.println("s0 = "+s0);
         double s1 = Polynomial.of(1.002737909, 0).at(t);
+        System.out.println("s1 = "+s1);
+        System.out.println("s0+s1 = " + (s0+s1));
 
         // Reduce s0 + s1 to the interval [0, 24[ and convert it to rad
         return Angle.ofHr(RightOpenInterval.of(0, 24).reduce(s0 + s1));
@@ -51,8 +59,8 @@ public final class SiderealTime {
      * @param where position of observation
      * @return sidereal time of this point of observation, with respect to the given position and J2000
      */
-    // TODO: 29/02/2020 check access
+    // TODO: 29/02/2020 check access and interval
     public static double local(ZonedDateTime when, GeographicCoordinates where) {
-        return greenwich(when) + where.lon();
+        return RightOpenInterval.of(0,Angle.TAU).reduce(greenwich(when) + where.lon());
     }
 }
--- test/ch/epfl/rigel/astronomy/SiderealTimeTest.java
+++ test/ch/epfl/rigel/astronomy/SiderealTimeTest.java
@@ -14,9 +14,14 @@ public class SiderealTimeTest {
 
     @Test
     void greenwichWorks() {
-        assertEquals(1.21762685685550, SiderealTime.greenwich(ZonedDateTime.of(LocalDate.of(1980, Month.APRIL, 22), LocalTime.of(14, 36, 51), ZoneOffset.UTC)));
-        assertEquals(20.516202146767, SiderealTime.greenwich(ZonedDateTime.of(LocalDate.of(2001, Month.JANUARY, 27), LocalTime.of(12, 0, 0), ZoneOffset.UTC)));
-        assertEquals(0.07288478193602, SiderealTime.greenwich(ZonedDateTime.of(LocalDate.of(2001, Month.SEPTEMBER, 11), LocalTime.of(8, 14, 0), ZoneOffset.UTC)));
-        assertEquals(2.47403226856777, SiderealTime.greenwich(ZonedDateTime.of(LocalDate.of(2004, Month.SEPTEMBER, 23), LocalTime.of(11, 0, 0), ZoneOffset.UTC)));
+
     }
+
+    @Test
+    void greenwichWorksOnTrivialCase() {
+        assertEquals(4.894961211636988, SiderealTime.greenwich(ZonedDateTime.of(LocalDate.of(2000, Month.JANUARY, 1),
+                LocalTime.of(12, 0),
+                ZoneOffset.UTC)));
+    }
+
 }
