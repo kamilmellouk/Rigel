0c179bb5ada4cef3040f3b37fbfcbf2e98484adf
Bastien Faivre <bastien.faivre@epfl.ch>
2020-03-02 09:37:49 +0100

    test

---
+++ test/ch/epfl/rigel/coordinates/EquatorialToHorizontalConversionTest.java
@@ -0,0 +1,18 @@
+package ch.epfl.rigel.coordinates;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Bastien Faivre
+ * 02/03/2020
+ */
+public class EquatorialToHorizontalConversionTest {
+
+    @Test
+    void ConversionWorksOnTrivialCases() {
+
+    }
+
+}
