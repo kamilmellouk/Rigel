f7ef7be7f6272ab763c62e5b3cebe69310623cc5
Bastien Faivre <bastien.faivre@epfl.ch>
2020-03-03 11:16:31 +0100

    SiderealTimeTest finished

--- test/ch/epfl/rigel/astronomy/MySiderealTimeTest.java
+++ test/ch/epfl/rigel/astronomy/MySiderealTimeTest.java
@@ -1,5 +1,6 @@
 package ch.epfl.rigel.astronomy;
 
+import ch.epfl.rigel.coordinates.GeographicCoordinates;
 import ch.epfl.rigel.math.Angle;
 import org.junit.jupiter.api.Test;
 
@@ -27,6 +28,26 @@ public class MySiderealTimeTest {
         assertEquals(4.894961211636988, SiderealTime.greenwich(ZonedDateTime.of(LocalDate.of(2000, Month.JANUARY, 1),
                 LocalTime.of(12, 0),
                 ZoneOffset.UTC)));
+    }
+
+    @Test
+    void localWorks() {
+        assertEquals(Angle.ofHr(4.668119327)+Math.PI/4, SiderealTime.local(ZonedDateTime.of(
+                LocalDate.of(1980, Month.APRIL, 22),
+                LocalTime.of(14, 36, 51, (int) 6.7e8),
+                ZoneOffset.UTC),
+                GeographicCoordinates.ofDeg(45,0)),
+                1e-10);
+    }
+
+    @Test
+    void localWorksOnTrivialCase() {
+        assertEquals(Angle.ofHr(4.668119327), SiderealTime.local(ZonedDateTime.of(
+                LocalDate.of(1980, Month.APRIL, 22),
+                LocalTime.of(14, 36, 51, (int) 6.7e8),
+                ZoneOffset.UTC),
+                GeographicCoordinates.ofDeg(0,0)),
+                1e-10);
     }
 
 }
