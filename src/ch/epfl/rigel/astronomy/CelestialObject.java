fbfc9b69b9a0a92fb48103979f9816ecd8fc3392
Bastien Faivre <bastien.faivre@epfl.ch>
2020-03-09 22:01:51 +0100

    CelestialObject checked

--- src/ch/epfl/rigel/astronomy/CelestialObject.java
+++ src/ch/epfl/rigel/astronomy/CelestialObject.java
@@ -2,10 +2,13 @@ package ch.epfl.rigel.astronomy;
 
 import ch.epfl.rigel.coordinates.EquatorialCoordinates;
 
+import java.util.Objects;
+
 /**
  * @author Bastien Faivre (310929)
  * @author Kamil Mellouk (312327)
  */
+
 public abstract class CelestialObject {
 
     // Attributes used to describe every celestial object
@@ -15,39 +18,57 @@ public abstract class CelestialObject {
     private float magnitude;
 
     /**
-     * Constructing a new Celestial Object (in subclasses)
-     * @param name of the celestial object
-     * @param equatorialPos (not null) equatorial coordinates of the celestial object
-     * @param angularSize (non negative) angular size of the celestial object
-     * @param magnitude magnitude of the celestial object
+     * Constructor of a new Celestial Object (in subclasses)
+     *
+     * @param name          the name of the celestial object
+     * @param equatorialPos (not null) the equatorial coordinates of the celestial object
+     * @param angularSize   (non negative) the angular size of the celestial object
+     * @param magnitude     the magnitude of the celestial object
      */
     CelestialObject(String name, EquatorialCoordinates equatorialPos, float angularSize, float magnitude) {
-        if(angularSize < 0 || equatorialPos == null) throw new IllegalArgumentException();
+        // TODO: 09/03/2020 check angularSize == 0 ?
+        // check exception
+        if (angularSize < 0) {
+            throw new IllegalArgumentException();
+        }
 
-        this.name = name;
-        this.equatorialPos = equatorialPos;
+        this.name = Objects.requireNonNull(name);
+        this.equatorialPos = Objects.requireNonNull(equatorialPos);
         this.angularSize = angularSize;
         this.magnitude = magnitude;
     }
 
+    /**
+     * @return the name of the celestial object
+     */
     public String name() {
         return name;
     }
 
-    public EquatorialCoordinates equatorialPos() {
-        return equatorialPos;
-    }
-
+    /**
+     * @return the angular size of the celestial object
+     */
     public double angularSize() {
         return angularSize;
     }
 
+    /**
+     * @return the magnitude of the celestial object
+     */
     public double magnitude() {
         return magnitude;
     }
 
     /**
+     * @return the equatorial coordinates of the celestial object
+     */
+    public EquatorialCoordinates equatorialPos() {
+        return equatorialPos;
+    }
+
+    /**
      * Displaying information on the Celestial object (redefined in subclasses)
+     *
      * @return String displaying the wanted information
      */
     public String info() {
