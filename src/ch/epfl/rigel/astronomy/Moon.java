517332858c5e053e6f35cad757b5dc43a2e32e47
Bastien Faivre <bastien.faivre@epfl.ch>
2020-03-09 22:11:18 +0100

    Moon checked

--- src/ch/epfl/rigel/astronomy/Moon.java
+++ src/ch/epfl/rigel/astronomy/Moon.java
@@ -1,5 +1,6 @@
 package ch.epfl.rigel.astronomy;
 
+import ch.epfl.rigel.Preconditions;
 import ch.epfl.rigel.coordinates.EquatorialCoordinates;
 import ch.epfl.rigel.math.ClosedInterval;
 
@@ -9,27 +10,30 @@ import java.util.Locale;
  * @author Bastien Faivre (310929)
  * @author Kamil Mellouk (312327)
  */
+
 public final class Moon extends CelestialObject {
 
     // Attribute specific to the Moon
-    private float phase;
+    // TODO: 09/03/2020 check the immutability
+    private final float phase;
 
     /**
-     * Constructing a Moon
-     * @param equatorialPos (not null) equatorial coordinates of the Moon
-     * @param angularSize (non negative) angular size of the Moon
-     * @param magnitude magnitude of the Moon
-     * @param phase (contained in [0, 1]) MoonPhase
+     * Constructor of a Moon
+     *
+     * @param equatorialPos (not null) the equatorial coordinates of the Moon
+     * @param angularSize   (non negative) the angular size of the Moon
+     * @param magnitude     the magnitude of the Moon
+     * @param phase         (contained in [0, 1]) the MoonPhase
      */
-    Moon(EquatorialCoordinates equatorialPos, float angularSize, float magnitude, float phase) {
+    public Moon(EquatorialCoordinates equatorialPos, float angularSize, float magnitude, float phase) {
         super("Lune", equatorialPos, angularSize, magnitude);
-        if(!ClosedInterval.of(0,1).contains(phase)) throw new IllegalArgumentException();
 
-        this.phase = phase;
+        // TODO: 09/03/2020 check the cast
+        this.phase = (float) Preconditions.checkInInterval(ClosedInterval.of(0, 1), phase);
     }
 
     @Override
     public String info() {
-        return String.format(Locale.ROOT,super.name() + " (%.1f%%)", phase*100);
+        return String.format(Locale.ROOT, "Lune (%.1f%%)", phase * 100);
     }
 }
