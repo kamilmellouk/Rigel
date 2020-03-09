fa254cf8230eab283a86ad7a54bdaae90701bca9
Bastien Faivre <bastien.faivre@epfl.ch>
2020-03-09 22:04:43 +0100

    Planet checked

--- src/ch/epfl/rigel/astronomy/Planet.java
+++ src/ch/epfl/rigel/astronomy/Planet.java
@@ -6,15 +6,19 @@ import ch.epfl.rigel.coordinates.EquatorialCoordinates;
  * @author Bastien Faivre (310929)
  * @author Kamil Mellouk (312327)
  */
+
 public final class Planet extends CelestialObject {
+
     /**
-     * Constructing a Planet
-     * @param name of the Planet
-     * @param equatorialPos (not null) equatorial coordinates of the Planet
-     * @param angularSize (non negative) angular size of the Planet
-     * @param magnitude magnitude of the Planet
+     * Constructor of a Planet
+     *
+     * @param name          the name of the planet
+     * @param equatorialPos (not null) the equatorial coordinates of the planet
+     * @param angularSize   (non negative) the angular size of the planet
+     * @param magnitude     the magnitude of the planet
      */
     Planet(String name, EquatorialCoordinates equatorialPos, float angularSize, float magnitude) {
         super(name, equatorialPos, angularSize, magnitude);
     }
+
 }
