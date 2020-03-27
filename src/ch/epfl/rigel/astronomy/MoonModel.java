d414382e242c144ae19f97429c4e359bd58dae7d
Bastien Faivre <bastien.faivre@epfl.ch>
2020-03-27 20:57:23 +0100

    javadoc add

--- src/ch/epfl/rigel/astronomy/MoonModel.java
+++ src/ch/epfl/rigel/astronomy/MoonModel.java
@@ -11,12 +11,12 @@ import ch.epfl.rigel.math.Angle;
 public enum MoonModel implements CelestialObjectModel<Moon> {
     MOON();
 
-    // Lunar values for the Epoch J2010
-    private final double meanLon = Angle.ofDeg(91.929336);
-    private final double perigMeanLon = Angle.ofDeg(130.143076);
-    private final double ascKnotLon = Angle.ofDeg(291.682547);
-    private final double orbitalIncl = Angle.ofDeg(5.145396);
-    private final double orbitalExc = 0.0549;
+    // Lunar constants for the Epoch J2010
+    private final static double meanLon = Angle.ofDeg(91.929336);
+    private final double meanLonAtPerigee = Angle.ofDeg(130.143076);
+    private final double ascendingNodeLon = Angle.ofDeg(291.682547);
+    private final double orbitInclination = Angle.ofDeg(5.145396);
+    private final double orbitEccentricity = 0.0549;
 
 
     @Override
@@ -26,7 +26,7 @@ public enum MoonModel implements CelestialObjectModel<Moon> {
 
         double orbitalLon = Angle.ofDeg(13.1763966) * daysSinceJ2010 + meanLon;
 
-        double meanMoonAnomaly = orbitalLon - Angle.ofDeg(0.1114041) * daysSinceJ2010 - perigMeanLon;
+        double meanMoonAnomaly = orbitalLon - Angle.ofDeg(0.1114041) * daysSinceJ2010 - meanLonAtPerigee;
 
         // Correction terms for the Sun's influence on the Moon's trajectory
         ///double evection = Angle.ofDeg(1.2739) * Math.sin(2*(orbitalLon - SunModel.));
