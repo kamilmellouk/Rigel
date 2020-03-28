e1dfb06f0f09e31f58a37f4322bc4c9cf37fb1d8
kamilmellouk <kamilmellouk@gmail.com>
2020-03-28 11:00:27 +0100

    framepad tests added all successful, except for Dec(e-13) and RaHr(e-11)

--- test/ch/epfl/rigel/astronomy/MyMoonModelTest.java
+++ test/ch/epfl/rigel/astronomy/MyMoonModelTest.java
@@ -20,4 +20,33 @@ class MyMoonModelTest {
         Moon moon = MoonModel.MOON.at(D, new EclipticToEquatorialConversion(date));
     }
 
+    @Test
+    void framepadTestAngularSize() {
+        assertEquals(0.009225908666849136, MoonModel.MOON.at(Epoch.J2010.daysUntil(ZonedDateTime.of(LocalDate.of(1979, 9, 1),LocalTime.of(0, 0),
+                ZoneOffset.UTC)), new EclipticToEquatorialConversion(ZonedDateTime.of(
+                LocalDate.of(1979, 9, 1),LocalTime.of(0, 0),ZoneOffset.UTC))).
+                angularSize());
+    }
+
+    @Test
+    void framepadTestInfo() {
+        assertEquals("Lune (22.5%)", MoonModel.MOON.at(Epoch.J2010.daysUntil(ZonedDateTime.of(LocalDate.of(2003, 9, 1),LocalTime.of(0, 0),
+                ZoneOffset.UTC)), new EclipticToEquatorialConversion(ZonedDateTime.of( LocalDate.of(2003, 9, 1),
+                LocalTime.of(0, 0),ZoneOffset.UTC))).
+                info() );
+    }
+
+    @Test
+    void framepadTestDec() {
+        assertEquals(-0.20114171346019355, MoonModel.MOON.at(-2313,
+                new EclipticToEquatorialConversion(ZonedDateTime.of(LocalDate.of(2003,  Month.SEPTEMBER, 1),LocalTime.of(0,0), ZoneOffset.UTC))).equatorialPos().dec(),
+                1e-13);
+    }
+
+    @Test
+    void framepadTestRaHr() {
+        assertEquals(14.211456457836277, MoonModel.MOON.at(-2313,
+                      new EclipticToEquatorialConversion(ZonedDateTime.of(LocalDate.of(2003,  Month.SEPTEMBER, 1),LocalTime.of(0,0), ZoneOffset.UTC))).equatorialPos().raHr(),
+                      1e-11);
+    }
 }
