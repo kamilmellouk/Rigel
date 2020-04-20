b8e05d9cdc19d09aba7ac61da19b6b4669de4642
kamilmellouk <kamilmellouk@gmail.com>
2020-04-20 13:30:45 +0000

    small change

--- src/ch/epfl/rigel/gui/SkyCanvasPainter.java
+++ src/ch/epfl/rigel/gui/SkyCanvasPainter.java
@@ -76,20 +76,17 @@ public class SkyCanvasPainter {
         ctx.setLineWidth(1);
 
         double[] transformedStarPositions = new double[sky.starPositions().length];
-        planeToCanvas.transform2DPoints(sky.starPositions(), 0, transformedStarPositions, 0, sky.starPositions().length/2);
+        planeToCanvas.transform2DPoints(sky.starPositions(), 0, transformedStarPositions, 0, sky.stars().size());
 
         for (Asterism asterism : sky.asterisms()) {
             ctx.beginPath();
-
             Point2D currentPos = new Point2D(
                     transformedStarPositions[sky.asterismIndices(asterism).get(0)*2],
                     transformedStarPositions[sky.asterismIndices(asterism).get(0)*2 + 1]
             );
-
-            Point2D nextPos;
-
             ctx.moveTo(currentPos.getX(), currentPos.getY());
 
+            Point2D nextPos;
             for (Star star : asterism.stars()) {
                 nextPos = new Point2D(
                         transformedStarPositions[sky.stars().indexOf(star)*2],
