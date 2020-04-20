a4c03ce38cac8133feb3735512a5cd7b6b184b91
Bastien Faivre <bastien.faivre@epfl.ch>
2020-04-20 15:48:46 +0200

    javadoc

--- src/ch/epfl/rigel/gui/TimeAccelerator.java
+++ src/ch/epfl/rigel/gui/TimeAccelerator.java
@@ -12,10 +12,23 @@ import java.time.temporal.ChronoUnit;
 @FunctionalInterface
 public interface TimeAccelerator {
 
+    /**
+     * Return a continuous time accelerator
+     *
+     * @param acceleratingFactor the given accelerating factor
+     * @return a continuous time accelerator
+     */
     static TimeAccelerator continuous(int acceleratingFactor) {
         return (initialSimulatedTime, elapsedRealTime) -> initialSimulatedTime.plus(elapsedRealTime * acceleratingFactor, ChronoUnit.NANOS);
     }
 
+    /**
+     * Return a discrete time accelerator
+     *
+     * @param advancementFrequency the advancement frequency
+     * @param step                 the discrete step
+     * @return a discrete time accelerator
+     */
     static TimeAccelerator discrete(int advancementFrequency, Duration step) {
         return (initialSimulatedTime, elapsedRealTime) -> initialSimulatedTime.plus((long) Math.floor(advancementFrequency * elapsedRealTime) * step.get(ChronoUnit.NANOS), ChronoUnit.NANOS);
     }
