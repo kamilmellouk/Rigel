3aa841af90cf6e26fd34155eee92568a4cbbbb34
Bastien Faivre <bastien.faivre@epfl.ch>
2020-04-28 14:14:59 +0200

    part 10

--- src/ch/epfl/rigel/gui/SkyCanvasManager.java
+++ src/ch/epfl/rigel/gui/SkyCanvasManager.java
@@ -27,7 +27,7 @@ import java.util.Optional;
 public class SkyCanvasManager {
 
     private final ObservableValue<ObservedSky> observedSky;
-    private final ObjectProperty<StereographicProjection> projection = new SimpleObjectProperty<>(null);
+    private final ObservableValue<StereographicProjection> projection;
     private final ObservableValue<Transform> planeToCanvas;
     private final ObjectProperty<CartesianCoordinates> mousePosition = new SimpleObjectProperty<>(null);
     private final ObservableValue<Optional<CelestialObject>> objectUnderMouse;
@@ -49,13 +49,28 @@ public class SkyCanvasManager {
                             ViewingParametersBean viewingParametersBean) {
 
         Canvas canvas = new Canvas();
+        SkyCanvasPainter painter = new SkyCanvasPainter(canvas);
+
+        projection = Bindings.createObjectBinding(
+                () -> new StereographicProjection(viewingParametersBean.getCenter()),
+                viewingParametersBean.centerProperty()
+        );
+
+        observedSky = Bindings.createObjectBinding(
+                () -> new ObservedSky(dateTimeBean.getZonedDateTime(), observerLocationBean.getCoordinates(), projection.getValue(), starCatalogue),
+                dateTimeBean.dateProperty(),
+                dateTimeBean.timeProperty(),
+                dateTimeBean.zoneProperty(),
+                observerLocationBean.coordinatesProperty(),
+                projection
+        );
 
         // TODO: 28/04/2020 How do we create the Tranform ?
-        /*
         planeToCanvas = Bindings.createObjectBinding(
-                () -> ,
-                projection, canvas.getWidth(), canvas.getHeight(), viewingParametersBean.getFieldOfViewDeg()
-        );*/
+                () -> Transform.affine(1300, 0, 0, -1300, 400, 300),
+                projection, canvas.widthProperty(), canvas.heightProperty(), viewingParametersBean.fieldOfViewDegProperty()
+        );
+
 
         // TODO: 28/04/2020 what is 10 units on the canvas ?
         objectUnderMouse = Bindings.createObjectBinding(
@@ -85,5 +100,62 @@ public class SkyCanvasManager {
                 mouseHorizontalPosition
         );
 
+    }
+
+    // TODO: 28/04/2020 Any setter for these 3 properties?
+
+    /**
+     * Getter for the property azimuth in deg of the mouse
+     *
+     * @return the property azimuth in deg of the mouse
+     */
+    public ObservableDoubleValue mouseAzDegProperty() {
+        return mouseAzDeg;
+    }
+
+    /**
+     * Getter for the azimuth in deg of the mouse
+     *
+     * @return the azimuth in deg of the mouse
+     */
+    public double getMouseAzDeg() {
+        return mouseAzDeg.get();
+    }
+
+
+    /**
+     * Getter for the property altitude in deg of the mouse
+     *
+     * @return the property altitude in deg of the mouse
+     */
+    public ObservableDoubleValue mouseAltDegProperty() {
+        return mouseAltDeg;
+    }
+
+    /**
+     * Getter for the altitude in deg of the mouse
+     *
+     * @return the altitude in deg of the mouse
+     */
+    public double getMouseAltDeg() {
+        return mouseAltDeg.get();
+    }
+
+    /**
+     * Getter for the property object under mouse
+     *
+     * @return the property object under mouse
+     */
+    public ObservableValue<Optional<CelestialObject>> objectUnderMouseProperty() {
+        return objectUnderMouse;
+    }
+
+    /**
+     * Getter for the object under mouse
+     *
+     * @return the object under mouse
+     */
+    public Optional<CelestialObject> getObjectUnderMouse() {
+        return objectUnderMouse.getValue();
     }
 }
