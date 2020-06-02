1dbb648862494a19ef15ffa6fc95239e0992ba52
Bastien Faivre <bastien.faivre@epfl.ch>
2020-06-02 13:41:49 +0200

    painter

--- src/ch/epfl/rigel/gui/SkyCanvasPainter.java
+++ src/ch/epfl/rigel/gui/SkyCanvasPainter.java
@@ -15,7 +15,6 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.ArcType;
 import javafx.scene.transform.Transform;
 
 import java.util.List;
